a4669b340525294972ba51448895f6ae96cd3236
z <112498488@qq.com>
2018-05-29 14:43:59 +0700

    v04

--- movies_images.scala
+++ movies_images.scala
@@ -15,26 +15,25 @@ object movies_images extends App {
 	val extensions_movies= List(".3gp",".mp4")
 //	val  movie_names =	names_allinfolder.filter( name_one => extensions_movies.exists( extension_one => name_one.matches(s".*$extension_one$$")  ) )
 	val  movie_names_string_list =	names_allinfolder.split("\n").filter(name_one => extensions_movies.exists( extension_one => name_one.matches(s".*$extension_one$$") ))
-	movie_names_string_list.toString.split("\n")
-	println(movie_names_string_list(1))
-	println(movie_names_string_list.indexOf("movie06.mp4"))
+//	movie_names_string_list.toString.split("\n")
+	println(movie_names_string_list(5))
+//	println(movie_names_string_list.indexOf("movie06.mp4"))
 //	  #2 catch fps
 	val f   ="/home/z/input_movies"
 	val x   ="movie06.mp4"
 //	val fps = new ProcessLogger
-	def fps_get(folder_path:String,movie_name:String) :Double={
+	def get_fps(folder_path:String,movie_name:String) :Double={
 			println(folder_path,movie_name)
 			val fps= s"ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $folder_path/$movie_name" !!
 		 	val fps_array = fps.toString.split("\\/")
 		   	fps_array(0).toDouble / fps_array(1).toDouble
-
 		}
-    println(	fps_get(f,movie_names_string_list(5)) )
-	val fps=s"ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $f/$x" !!
-//	val fps  = fps_command
-
- 	val fps_array = fps.toString.split("\\/")
-	fps_array(0).toDouble / fps_array(1).toDouble
+    println(	get_fps(f,movie_names_string_list(5)) )
+//	val fps=s"ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $f/$x" !!
+////	val fps  = fps_command
+//
+// 	val fps_array = fps.toString.split("\\/")
+//	fps_array(0).toDouble / fps_array(1).toDouble
 
 
 
